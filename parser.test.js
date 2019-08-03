a15a858c4afaf482e87f86098d540bf747b5b73e
Björn Dahlgren <bjorn@dahlgren.at>
2019-08-03 18:08:45 +0200

    Support both sharedfiles and workshop urls

--- parser.test.js
+++ parser.test.js
@@ -1,7 +1,9 @@
 const { parseSteamId, parseSteamUrls } = require('./parser')
 
-const urlWithPathId = 'https://steamcommunity.com/sharedfiles/filedetails/123456789'
-const urlWithQueryId = 'https://steamcommunity.com/sharedfiles/filedetails/?id=123456789'
+const sharedFilesUrlWithPathId = 'https://steamcommunity.com/sharedfiles/filedetails/123456789'
+const sharedFilesUrlWithQueryId = 'https://steamcommunity.com/sharedfiles/filedetails/?id=123456789'
+const workshopUrlWithPathId = 'https://steamcommunity.com/workshop/filedetails/123456789'
+const workshopUrlWithQueryId = 'https://steamcommunity.com/workshop/filedetails/?id=123456789'
 
 describe('parseSteamId', () => {
   test('return undefined for non url', () => {
@@ -13,11 +15,11 @@ describe('parseSteamId', () => {
   })
 
   test('return id in url with path id', () => {
-    expect(parseSteamId(urlWithPathId)).toStrictEqual(123456789)
+    expect(parseSteamId(sharedFilesUrlWithPathId)).toStrictEqual(123456789)
   })
 
   test('return id in url with path id', () => {
-    expect(parseSteamId(urlWithQueryId)).toStrictEqual(123456789)
+    expect(parseSteamId(sharedFilesUrlWithQueryId)).toStrictEqual(123456789)
   })
 })
 
@@ -30,23 +32,31 @@ describe('parseSteamUrls', () => {
     expect(parseSteamUrls('https://store.steampowered.com')).toStrictEqual([])
   })
 
-  test('return array with url when entire message is the url with path id', () => {
-    expect(parseSteamUrls(urlWithPathId)).toStrictEqual([urlWithPathId])
+  test('return array with url when entire message is the shared files url with path id', () => {
+    expect(parseSteamUrls(sharedFilesUrlWithPathId)).toStrictEqual([sharedFilesUrlWithPathId])
   })
 
-  test('return array with url when entire message is the url with query id', () => {
-    expect(parseSteamUrls(urlWithQueryId)).toStrictEqual([urlWithQueryId])
+  test('return array with url when entire message is the shared files url with query id', () => {
+    expect(parseSteamUrls(sharedFilesUrlWithQueryId)).toStrictEqual([sharedFilesUrlWithQueryId])
+  })
+
+  test('return array with url when entire message is the workshop url with path id', () => {
+    expect(parseSteamUrls(workshopUrlWithPathId)).toStrictEqual([workshopUrlWithPathId])
+  })
+
+  test('return array with url when entire message is the workshop url with query id', () => {
+    expect(parseSteamUrls(workshopUrlWithQueryId)).toStrictEqual([workshopUrlWithQueryId])
   })
 
   test('return array with urls when message is two paths with id', () => {
-    expect(parseSteamUrls(`${urlWithPathId} and ${urlWithPathId}`)).toStrictEqual([urlWithPathId, urlWithPathId])
+    expect(parseSteamUrls(`${sharedFilesUrlWithPathId} and ${sharedFilesUrlWithPathId}`)).toStrictEqual([sharedFilesUrlWithPathId, sharedFilesUrlWithPathId])
   })
 
   test('return array with urls when message is two queries with id', () => {
-    expect(parseSteamUrls(`${urlWithQueryId} and ${urlWithQueryId}`)).toStrictEqual([urlWithQueryId, urlWithQueryId])
+    expect(parseSteamUrls(`${sharedFilesUrlWithQueryId} and ${sharedFilesUrlWithQueryId}`)).toStrictEqual([sharedFilesUrlWithQueryId, sharedFilesUrlWithQueryId])
   })
 
   test('return array with urls when message is one path with id and one query with id', () => {
-    expect(parseSteamUrls(`${urlWithPathId} and ${urlWithQueryId}`)).toStrictEqual([urlWithPathId, urlWithQueryId])
+    expect(parseSteamUrls(`${sharedFilesUrlWithPathId} and ${sharedFilesUrlWithQueryId}`)).toStrictEqual([sharedFilesUrlWithPathId, sharedFilesUrlWithQueryId])
   })
 })
